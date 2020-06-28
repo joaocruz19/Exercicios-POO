19c6428affd3912f75dcf205c0ae5a1ec99bf860
joaocruz19 <joaovictorcruz82@gmail.com>
2020-06-28 12:29:06 -0300

    list 6 completed

--- lista-6/Cliente.java
+++ lista-6/Cliente.java
@@ -1,8 +1,23 @@
 package petsitter;
+import java.util.ArrayList;
 
 public class Cliente extends Cadastro {
-	
-	public Cliente(String name, String email, String cpf, Endereco address, String phone, Animal animal) {
-		super(name, email, cpf, address, phone, animal);
+	private List<Animal> animals = new ArrayList<Animal>();
+
+	public Cliente(String name, String email, String cpf, Endereco address, String phone) {
+		super(name, email, cpf, address, phone);
+	}
+
+	public void addAnimal(Animal animal) {
+		if(animals.length >= 5) {
+			System.out.println("Você possui o limite de animais para serem cadastrados!");
+		} else {
+			animals.add(animal);
+			System.out.println("Animal adicionado com sucesso.");
+		}
+	}
+
+	public void removeAnimal(Animal animal) {
+		animals.remove(animal);
 	}
 }
--- lista-6/Menu.java
+++ lista-6/Menu.java
@@ -1,21 +1,21 @@
-package petsitter;
-
 import java.util.Scanner;
 
 public class Menu {
 	public static void renderOptions() {
 		System.out.println("Cadastro: ");
-		System.out.println("\n1 - Cadastar");
-		System.out.println("\n2 - Editar Cadastro");
-		System.out.println("\n3 - Excluir");
-		System.out.println("\n4 - Consultar");
-		System.out.println("\n5 - Sair");
-		System.out.println("\n - Opção: ");
+		System.out.println("1 - Cadastar");
+		System.out.println("2 - Cadastrar animal");
+		System.out.println("3 - Excluir");
+		System.out.println("4 - Raças mais comuns");
+		System.out.println("5 - Sair");
+		System.out.println(" - Opcao: ");
 	}
 
 	public static void main(String[] args) {
 		int option;
 		Scanner input = new Scanner(System.in);
+		String operationType;
+		Controller controller = new Controller();
 		
 		do {
 			renderOptions();
@@ -23,24 +23,31 @@ public class Menu {
 			
 			switch(option) {
 				case 1:
-					//Register;
+					System.out.print("Deseja cadastrar cliente ou profissional? ");
+					operationType = input.next();
+					controller.register(operationType);
 					break;
 				case 2:
-					//Editar;
+					System.out.print("Nome do cliente: ");
+					String customerName = input.next()\;
+					controller.registerAnimal(customerName);
 					break;
 				case 3:
-					//Excluir();
+					System.out.print("Deseja deletar cliente ou profissional? ");
+					operationType = input.next();
+					controller.delete(operationType);
 					break;
 				case 4:
-					//Consultar();
+					controller.showCommonBreeds();
 					break;
 				case 5:
-					//
+					System.out.println("Saindo...");
 					break;
 				default:
-					System.out.println("Opção inválida");
+					System.out.println("Opcao invalida");
 			}
 			
 		} while(option != 5);
+		input.close();
 	}
 }
